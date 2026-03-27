63d4f4c67186e262d6d9028cd8cd2ccd0a4e9a65
youngcharleslucas <charles.lucas.young@gmail.com>
2026-03-27 14:40:10 -0600

    feat: initial commit

---
+++ src/features/Blah/pages/Blah.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+export default function Blah() {
+  return (
+    <div>
+      <h1>Blah</h1>
+      <section>
+        <div>
+          <Button variant="outline">Hello world</Button>
+        </div>
+      </section>
+    </div>
+  )
+}
---
+++ src/features/NWO/pages/NWO.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/v1/button'
+export default function NWO() {
+  return (
+    <div>
+      <h1>NWO</h1>
+      <section>
+        <div>
+          <Button variant='outline'>Wrapper</Button>
+        </div>
+      </section>
+    </div>
+  )
+}
